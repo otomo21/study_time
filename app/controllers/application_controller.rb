83ba57b871c3ec135a1b585452d73692477128c5
otomo21 <k1bbs2str@gmail.com>
2019-07-24 02:12:28 +0000

    テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "勉強時間を記録していきます"
+    render html: "勉強時間を記録していきます。。。"
   end
 end
